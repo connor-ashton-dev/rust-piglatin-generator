caaced92cfa8db5c704482d140c42f4df61de6c0
Connor Ashton <cjconnorashton@gmail.com>
2023-11-28 19:46:50 -0700

    first commit - rough start to translator
    Still need to work on capitalization and consonant things. Like if word starts with vowel.

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+fn main() {
+    let my_text = "hi my name is connor";
+    let seperated_words: Vec<&str> = my_text.split_whitespace().collect();
+    let phrase = translate(&seperated_words);
+    println!("{}", phrase);
+}
+
+fn translate(words: &Vec<&str>) -> String {
+    let mut sentence = String::new();
+    for word in words {
+        let new_word = turn_word_to_piglatin(word);
+        sentence.push_str(&new_word)
+    }
+    return sentence;
+}
+
+fn turn_word_to_piglatin(w: &str) -> String {
+    let mut word = String::new();
+    let mut curr_idx = 0;
+    for c in w.chars() {
+        if is_consonant(&c) {
+            let consonant_slice = &w[..curr_idx + 1];
+            let mut front = consonant_slice.to_string();
+            if consonant_slice.chars().all(|c| c.is_uppercase()) {
+                front.push_str("AY");
+            } else {
+                front.push_str("ay");
+            }
+            let rest = &w[curr_idx + 1..];
+            word = format!("{rest}{front} ");
+            break;
+        }
+        curr_idx += 1;
+    }
+    return word;
+}
+
+fn is_consonant(c: &char) -> bool {
+    if c.is_numeric() {
+        return false;
+    }
+
+    let lowercase = c.to_ascii_lowercase();
+    match lowercase {
+        'a' | 'e' | 'i' | 'o' | 'u' => false,
+        _ => true,
+    }
+}
